4823984916b941d940792944eda3fa953d3c7614
Grzegorz Cyran <grzegorz.cyran@interia.pl>
2023-10-15 17:36:49 +0200

    Samples modified

--- src/edu/gc/basics/Samples.java
+++ src/edu/gc/basics/Samples.java
@@ -10,12 +10,23 @@ public class Samples {
             }
             System.out.println(); //pusty sout przechodzi do kolejnej linii
         }
+
+        printStars(8);
     }
 
     //wyświetlamy liczby parzyste lub nieparzyste dla zakresu do 10
     public static void printEvensOrOdds(int startIndex) {
         for(int i = 1; i <= 10; i=i+2) {
             System.out.println("i: " + i);
+        }
+    }
+
+    public static void printStars(int startIndex) {
+        for(int i = 1; i <= 10; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("x");
+            }
+            System.out.println(); //pusty sout przechodzi do kolejnej linii
         }
     }
 }
